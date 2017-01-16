dfa2b967119b3b5b7e6221119c71a69a32e0116f
ciderpunx <charlie@jura.newint.org>
2017-01-16 10:05:19 -0500

    BUGFIX: Glitchy loading circle in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,12 @@
 			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+			// $body.addClass('is-loading');
 
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
-				}, 0);
+				}, 2);
 			});
 
 		// Touch mode.
